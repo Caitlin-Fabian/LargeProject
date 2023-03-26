478f00e972f6a4c2275519f91c153c54e94177b4
brysonPaul <brysonpaul1870@gmail.com>
2023-03-25 23:49:58 -0400

    unit test framework set up under /tests dir. take a look and read comments/ msg me if you need assistance :)

--- api.js
+++ api.js
@@ -3,16 +3,6 @@ require('express');
 require('mongodb');
 
 exports.setApp = function (app, client) {
-    // app.post('/api/addcard', async (req, res, next) => {
-    //     // incoming: userId, color
-    //     // outgoing: error
-    //     var error = '';
-    //     const { userId, card } = req.body;
-    //     // TEMP FOR LOCAL TESTING.
-    //     cardList.push(card);
-    //     var ret = { error: error };
-    //     res.status(200).json(ret);
-    // });
     app.post('/api/login', async (req, res, next) => {
         // incoming: login, password
         // outgoing: id, firstName, lastName, error
@@ -34,23 +24,6 @@ exports.setApp = function (app, client) {
         var ret = { id: id, Name: Name, score: score, error: '' };
         res.status(200).json(ret);
     });
-    // app.post('/api/searchcards', async (req, res, next) => {
-    //     // incoming: userId, search
-    //     // outgoing: results[], error
-    //     var error = '';
-    //     const { userId, search } = req.body;
-    //     var _search = search.toLowerCase().trim();
-    //     var _ret = [];
-    //     for (var i = 0; i < cardList.length; i++) {
-    //         var lowerFromList = cardList[i].toLocaleLowerCase();
-    //         if (lowerFromList.indexOf(_search) >= 0) {
-    //             _ret.push(cardList[i]);
-    //         }
-    //     }
-    //     var ret = { results: _ret, error: '' };
-    //     res.status(200).json(ret);
-    // });
-
     //register API
 
     app.post('/api/register', async (req, res, next) => {
