83c40a7e8bf0a1d00090335bc40c48583a3d2404
Caitlin Fabian <caitlinfstar@knights.ucf.edu>
2023-03-29 23:36:41 -0400

    Scoreboard update

--- frontend/src/components/Scoreboard.js
+++ frontend/src/components/Scoreboard.js
@@ -6,10 +6,14 @@ import one from '../assets/Leader1.svg';
 import two from '../assets/Leader2.svg';
 import three from '../assets/Leader3.svg';
 
+import TopPlayer from './TopPlayers';
+
 function Scoreboard() {
     const [message, setMessage] = useState('');
+    const [showPlayers, setShowPlayers] = useState(false);
     const [topPlayers, setTopPlayers] = useState([]);
     var bp = require('./Path.js');
+    var Topscores;
     const showleaderBoard = async () => {
         try {
             const response = await fetch(bp.buildPath('api/leaderboard'), {
@@ -23,7 +27,7 @@ function Scoreboard() {
                 setMessage('This are no players');
             } else {
                 console.log(res.userList);
-
+                setShowPlayers(true);
                 setTopPlayers(res.userList);
             }
         } catch (e) {
@@ -31,46 +35,15 @@ function Scoreboard() {
             return;
         }
     };
+
+    if (showPlayers) {
+        Topscores = <TopPlayer topPlayers={topPlayers} />;
+    }
+
     useEffect(() => {
         showleaderBoard();
     }, []);
 
-    return (
-        <Container className="scoreboard-container justify-content-center">
-            <Row className="position-relative d-flex align-items-center">
-                <Col className="d-flex justify-content-center">
-                    <img
-                        style={{ width: '60%' }}
-                        src={one}
-                        alt="Ranking Decoration"
-                    ></img>
-                </Col>
-                <Col className="position-absolute d-flex justify-content-center align-items-center">
-                    <div className="placement"></div>
-                </Col>
-            </Row>
-            <Row className="position-relative d-flex align-items-center">
-                <Col className="position-relative d-flex justify-content-center my-auto">
-                    <img
-                        style={{ width: '100%' }}
-                        src={two}
-                        alt="Ranking Decoration"
-                    ></img>
-                    <div
-                        style={{ top: '10%' }}
-                        className="position-absolute my-auto d-flex align-items-center placement"
-                    ></div>
-                </Col>
-                <Col className="position-relative d-flex justify-content-center my-auto">
-                    <img
-                        style={{ width: '90%' }}
-                        src={three}
-                        alt="Ranking Decoration"
-                    ></img>
-                    <div className="position-absolute d-flex align-items-center placement"></div>
-                </Col>
-            </Row>
-        </Container>
-    );
+    return <div>{Topscores}</div>;
 }
 export default Scoreboard;
