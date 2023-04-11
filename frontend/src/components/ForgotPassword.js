6a79f357760f62b764bb46be151757a9a43e50af
Caitlin <caitlinfstar@gmail.com>
2023-04-11 16:45:47 -0400

    Forgot password

--- frontend/src/components/ForgotPassword.js
+++ frontend/src/components/ForgotPassword.js
@@ -5,15 +5,47 @@ import * as Mdicons from 'react-icons/md';
 import Form from 'react-bootstrap/Form';
 
 const Email = () => {
-    const [token, setToken] = useState('');
     const [message, setMessage] = useState('');
+    const [email, setEmail] = useState('');
+    const [token, setToken] = useState('');
+    const [enterEmail, setEnterEmail] = useState(true);
+    const [verifyEmail, setVerifyEmail] = useState(false);
+    const [changePassword, setChangePassword] = useState(false);
     var bp = require('./Path.js');
 
     const handleOutput = (string) => {
         setToken(string);
     };
+    const sendEmail = async () => {
+        var obj = {
+            email: email,
+        };
 
-    const verifyEmail = async () => {
+        console.log(obj);
+        var js = JSON.stringify(obj);
+        console.log(js);
+        try {
+            const response = await fetch(
+                bp.buildPath('api/sendForgotPassword'),
+                {
+                    method: 'POST',
+                    body: js,
+                    headers: { 'Content-Type': 'application/json' },
+                }
+            );
+            var res = JSON.parse(await response.text());
+            console.log(res);
+            if (res.error === 'N/A') {
+                setEnterEmail(false);
+                setVerifyEmail(true);
+            } else {
+                setMessage(res.error);
+            }
+        } catch (e) {
+            console.log(e.toString());
+        }
+    };
+    const verify = async () => {
         var obj = {
             token: token,
         };
@@ -41,6 +73,11 @@ const Email = () => {
     const handleBack = () => {
         window.location.href = '/';
     };
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        setEmail(e.target.value);
+    };
     return (
         <div className="d-flex justify-content-center ">
             <div className="Email-container d-flex justify-content-center flex-column justify-content-around">
@@ -60,24 +97,74 @@ const Email = () => {
                     >
                         Password Recovery
                     </h1>
-                    <p>Please enter your email</p>
-                    <Form>
-                        <Form.Control
-                            className="w-75 d-flex justify-content-center"
-                            type="Name"
-                            placeholder="Email@email.com"
-                        />
-                    </Form>
-                    <div>
-                        <button
-                            className="clkbtn"
-                            onClick={verifyEmail}
-                        >
-                            Send Email
-                        </button>
-                        <br />
-                        <span id="loginResult">{message}</span>{' '}
-                    </div>
+
+                    {enterEmail && (
+                        <>
+                            <p>Please enter your email</p>
+                            <Form>
+                                <Form.Control
+                                    className="w-75 d-flex justify-content-center"
+                                    type="Email"
+                                    placeholder="Email@email.com"
+                                    onChange={handleChange}
+                                />
+                            </Form>
+                            <div>
+                                <button
+                                    className="clkbtn"
+                                    onClick={sendEmail}
+                                >
+                                    Send Email
+                                </button>
+                                <br />
+                                <span id="loginResult">{message}</span>{' '}
+                            </div>
+                        </>
+                    )}
+                    {verifyEmail && (
+                        <>
+                            <p>Please enter the code from your email</p>
+                            <div>
+                                <RICIBs
+                                    amount={5}
+                                    autoFocus
+                                    handleOutputString={handleOutput}
+                                    inputProps={[{ className: 'first-box' }]}
+                                    inputRegExp={/^[a-zA-Z0-9_.-]*$/}
+                                />
+                            </div>
+                            <button
+                                className="clkbtn"
+                                onClick={verify}
+                            >
+                                Verify Email
+                            </button>
+                        </>
+                    )}
+
+                    {changePassword && (
+                        <>
+                            <Form>
+                                <Form.Label>New Password</Form.Label>
+                                <Form.Control
+                                    className="w-75 d-flex justify-content-center"
+                                    type="Password"
+                                    placeholder="password"
+                                    onChange={handleChange}
+                                />
+                            </Form>
+                            <div>
+                                <button
+                                    className="clkbtn"
+                                    onClick={sendEmail}
+                                >
+                                    Send Email
+                                </button>
+                                <br />
+                                <span id="loginResult">{message}</span>{' '}
+                            </div>
+                        </>
+                    )}
                 </div>
             </div>
         </div>
